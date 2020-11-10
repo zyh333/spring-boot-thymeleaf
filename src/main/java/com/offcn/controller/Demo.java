c119557f961ec5e685bb10313f087f86a5512792
java-0713 <zhuyonghuilte@163.com>
2020-11-10 19:51:23 +0800

    这是增加注释

--- src/main/java/com/offcn/controller/Demo.java
+++ src/main/java/com/offcn/controller/Demo.java
@@ -4,6 +4,7 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("新增demo");
     }
+    //增加注释
     public void test(){
         System.out.println("新增测试方法");
     }
